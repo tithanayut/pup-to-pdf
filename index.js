5d98530d599d6d9363a20fe59d6cb62c4ff5d7b3
Thanayut T <b@thanayut.in.th>
2021-05-29 21:54:32 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const puppeteer = require("puppeteer");
+
+async function savePDF() {
+	const browser = await puppeteer.launch({ headless: true });
+
+	const page = await browser.newPage();
+	await page.goto("http://localhost/", {
+		waitUntil: "networkidle0",
+	});
+
+	const pdf = await page.pdf({ format: "A4" });
+	await browser.close();
+	return pdf;
+}
+
+console.log("Processing...");
+savePDF().then((pdf) => {
+	fs.writeFile("test.pdf", pdf, null, () => {
+		console.log("Done!");
+	});
+});
